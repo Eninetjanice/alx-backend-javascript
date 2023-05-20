5b03a92efacd81fded023cd5976d526b04e94286
Janeth Eni <enijaneth@gmail.com>
2023-05-20 10:50:33 +0100

    Deep equality & Post integration test

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,10 +1,38 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 7865;
 
+app.use(bodyParser.json());
+
 app.get('/', (_, res) => {
   res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id', (req, res) => {
+  const { id } = req.params;
+  if (isNaN(id) || id < 0) {
+    res.status(404).send('Invalid cart ID');
+  } else {
+    res.send(`Payment methods for cart ${id}`);
+  }
+});
+
+app.get('/available_payments', (_, res) => {
+  const paymentMethods = {
+    payment_methods: {
+      credit_cards: true,
+      paypal: false
+    }
+  };
+
+  res.json(paymentMethods);
+});
+
+app.post('/login', (req, res) => {
+  let { userName } = req.body;
+  res.status(200).send(`Welcome ${userName}`);
 });
 
 app.listen(PORT, () => {
--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const { expect } = require('chai');
 
-describe('API integration test', () => {
+describe('API Regex integration test', () => {
   const API_URL = 'http://localhost:7865';
 
   it('GET / returns correct response', (done) => {
@@ -11,4 +11,48 @@ describe('API integration test', () => {
       done();
     });
   });
+
+  it('GET /cart/:id return correct response for valid :id', (done) => {
+    request.get(`${API_URL}/cart/222`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 222');
+      done();
+    });
+  });
+
+  it('GET /cart/:id return 404 response when :id is a negative number', (done) => {
+    request.get(`${API_URL}/cart/-222`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+
+  it('GET /cart/:id return 404 response when :id is NOT a number', (done) => {
+    request.get(`${API_URL}/cart/de00`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+
+  it('POST /login returns 200 status code where :username is the value of the body variable userName', (done) => {
+    request.post(`${API_URL}/login`, {json: {userName: 'Betty'}}, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(res.body).to.be.equal('Welcome Betty');
+      done();
+    });
+  });
+
+  it('GET /available_payments return 200 response when userName is valid', (done) => {
+    request.get(`${API_URL}/available_payments`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(JSON.parse(res.body)).to.be.deep.equal({
+        payment_methods: {
+          credit_cards: true,
+          paypal: false
+        }
+      });
+      done();
+    });
+  });
+
 });
