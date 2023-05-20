45065080020ecf3a229ecfe9e3434466ad19a727
Janeth Eni <enijaneth@gmail.com>
2023-05-20 07:49:00 +0100

    Regex integration testing

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -7,6 +7,15 @@ app.get('/', (_, res) => {
   res.send('Welcome to the payment system');
 });
 
+app.get('/cart/:id', (req, res) => {
+  const { id } = req.params;
+  if (isNaN(id) || id < 0) {
+    res.status(404).send('Invalid cart ID');
+  } else {
+    res.send(`Payment methods for cart ${id}`);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`API available on localhost port ${PORT}`);
 });
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,13 +1,35 @@
 const request = require('request');
 const { expect } = require('chai');
 
-describe('API integration test', () => {
+describe('API Regex integration test', () => {
   const API_URL = 'http://localhost:7865';
 
   it('GET / returns correct response', (done) => {
     request.get(`${API_URL}/`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('GET /cart/:id return correct response for valid :id', (done) => {
+    request.get(`${API_URL}/cart/222`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 222');
+      done();
+    });
+  });
+
+  it('GET /cart/:id return 404 response when :id is a negative number', (done) => {
+    request.get(`${API_URL}/cart/-222`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+
+  it('GET /cart/:id return 404 response when :id is NOT a number', (done) => {
+    request.get(`${API_URL}/cart/de00`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
       done();
     });
   });
